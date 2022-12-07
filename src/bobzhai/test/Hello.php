2aeb48c9d82ad8f1feac2f2b7bcae7c29b667999
azhai2016 <1229991003@qq.com>
2022-12-07 08:02:04 +0800

    init_5

---
+++ src/bobzhai/test/Hello.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace bobzhai\test;
+
+class Hello{
+    public function __construct(){
+
+    }
+
+    public static function say() {
+        
+        echo 'Hi World!';
+    }
+}
+
+
+?>
